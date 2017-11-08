931a5f35960f99676b0ebf695750d5cae9273e2e
fiacre <fiacre.patrick@gmail.com>
2017-11-08 14:44:53 -0500

    flatten out app so that it can run as one simple pyton command

--- app.py
+++ app.py
@@ -1,13 +1,97 @@
-# -*- coding: utf-8 -*-
 from flask import Flask
-from url_resolver.marcxml_parse import MARCXmlParse
-from url_resolver.config import base_url, ns
 from flask import render_template, abort, redirect
 from urllib import request as req
 from urllib.parse import quote_plus
+from io import BytesIO
 import re
 import ssl
 import xml.etree.ElementTree as ET
+from pymarc import marcxml
+from urllib import parse
+
+base_url = 'https://digitallibrary.un.org'
+ns = '{http://www.loc.gov/MARC21/slim}'
+path = '/search'
+
+subject_re = re.compile(r'^\d{6,7}\s(?:unbis[nt])*\s*(.+)$')
+reldoc_re = re.compile(r'^([a-zA-Z0-9\/]+)(\(\d{4}\))$')
+
+
+class PageNotFoundException(Exception):
+    pass
+
+
+class MARCXmlParse:
+    '''
+        given a url, e.g.
+            https://digitallibrary.un.org/record/696939/export/xm
+        parse the xml via pymarc.parse_xml_to_array
+        use pymarc to pull out fields:  
+            author
+            notes
+            publisher
+            pubyear
+            subjects
+            title
+            document symbol
+            related documents
+    '''
+    def __init__(self, url):
+        resp = req.urlopen(url, context=ssl._create_unverified_context())
+        if resp.status != 200:
+            raise PageNotFoundException("Could not get data from {}".format(url)) 
+        self.xml_doc = BytesIO(resp.read())
+        r = marcxml.parse_xml_to_array(self.xml_doc, False, 'NFC')
+        self.record = r[0]
+
+    def author(self):
+        return self.record.author()
+
+    def title(self):
+        return self.record.title()
+
+    def subjects(self):
+        subjs = {}
+        for sub in self.record.subjects():
+            app.logger.debug("Subject: {}".format(sub.value()))
+            m = subject_re.match(sub.value())
+            if m:
+                search_string = parse.quote_plus(m.group(1))
+                query = "f1=subject&as=1&sf=title&so=a&rm=&m1=p&p1={}&ln=en".format(search_string)
+                subjs[m.group(1)] = base_url + path + '?' + query
+        app.logger.debug(subjs)
+        return subjs
+
+    def notes(self):
+        return [note.value() for note in self.record.notes()]
+
+    def publisher(self):
+        return self.record.publisher()
+
+    def pubyear(self):
+        return self.record.pubyear()
+
+    def document_symbol(self):
+        return self.record.document_symbol()
+
+    def related_documents(self):
+        docs = {}
+        for rel_doc in self.record.related_documents():
+            app.logger.debug("Related Doc: {}".format(rel_doc.value()))
+            m = reldoc_re.match(rel_doc.value())
+            if m:
+                rel_string = m.group(1) + '%20' + m.group(2)
+                docs[rel_doc.value()] = '/symbol/{}'.format(rel_string)
+            else:
+                docs[rel_doc.value()] = '/symbol/{}'.format(rel_doc.value())
+        return docs
+
+    def summary(self):
+        return self.record.summary()
+
+    def agenda(self):
+        return self.record.agenda()
+
 
 app = Flask(__name__)
 context = ssl._create_unverified_context()
@@ -131,3 +215,6 @@ def _fetch_xml_root(url_pattern, param):
         abort(404)
     root = ET.fromstring(xml)
     return root
+
+if __name__ == '__main__':
+    app.run(debug=True)
